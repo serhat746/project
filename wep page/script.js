745d7daf281e7bc4cffdee4e00c54f684b618e9a
serhat746 <84407622+serhat746@users.noreply.github.com>
2021-05-18 21:16:55 +0300

    Add files via upload

---
+++ wep page/script.js
@@ -0,0 +1,12 @@
+var k_username = "serhat";
+var k_password = "ıron165";
+ 
+
+var username=prompt ("kullanıcı adı :");
+var password=prompt ("parola giriniz")
+
+if((k_username==username) &&(k_password==password)){
+console.log("giriş yapıldı ");
+}else{
+    console.log("hatalı kullanıcı adı girdiniz")
+}
